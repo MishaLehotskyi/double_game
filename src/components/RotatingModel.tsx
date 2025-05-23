34affd6ecaf4c5712112110169be5c75488279be
MishaLehotskyi <eglet777@ukr.net>
2025-05-23 03:58:43 +0300

    mega bank 3d fix

--- src/components/RotatingModel.tsx
+++ src/components/RotatingModel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF, Bounds } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Suspense } from "react";
 
 function Model({ fileName }: { fileName: string }) {
