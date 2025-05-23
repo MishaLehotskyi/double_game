4f374d198320e63084babb65a7a938b6bc95ba03
MishaLehotskyi <eglet777@ukr.net>
2025-05-24 00:36:49 +0300

    clarification

--- src/app/mega-bank/page.tsx
+++ src/app/mega-bank/page.tsx
@@ -160,7 +160,7 @@ export default function StandardBank() {
           <Tooltip
             open={open}
             onClick={handleClick}
-            title={"Для участия в лорее Mega Bank переведите 500 токенов DBE на адрес 0x740B45a8E7C01AAFC6CD823e5a794F172eE9cCD0 Сеть BSC. После этого вы автоматически появитесь в окне участников с присвоенным номером билета"}
+            title={"Для участия в лорее Mega Bank переведите 500 токенов DBE на адрес 0x740B45a8E7C01AAFC6CD823e5a794F172eE9cCD0 Сеть BNB Smart Chain (BEP 20). После этого вы автоматически появитесь в окне участников с присвоенным номером билета"}
             disableFocusListener
             disableHoverListener
             disableTouchListener
